0eb050a79e4e680c50ffd28ae1b20fca33e0c250
Artur Prydatko <artyrpridatko@gmail.com>
2020-04-17 16:21:35 +0300

    Fixed style and add jest test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const App = () => {
-    return (
-        <div>
-            React Starter
-        </div>
-    )
+	return <div>React Starter</div>
 }
 
 export default App
